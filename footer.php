9aad8aaec106a56be30882a73ea99ef1161f1bda
thimh <thim.heider@gmail.com>
2014-02-20 10:56:11 +0100

    fonts

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
 		<div id="footer">
 			&copy; All rights reserved.
 			<br />
-			Deze site is een schoolopdracht.
-			<br />
-			Deze site is XHTML 1.1 en CSS 3 validated.
-			<p>
+			Deze site is een schoolopdracht en XHTML 1.1 en CSS 3 validated.
     			<a href="http://validator.w3.org/check?uri=referer"><img
       				src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" />
       			</a>
@@ -13,7 +10,6 @@
             			src="http://jigsaw.w3.org/css-validator/images/vcss"
             			alt="Valid CSS!" />
     			</a>
-  			</p>
 		</div>
 	</body>
 </html>
