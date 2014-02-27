4f09297b0c1c6c21f2efc9797e34009c3de0adb9
thimh <thim.heider@gmail.com>
2014-02-28 00:06:50 +0100

    Fancy background-text kleuren combi & poging tot sticky footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 		<div id="footer">
-			&copy; All rights reserved.
-			<!--
-			<br />
-			Deze site is een schoolopdracht en <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> 
-			en is <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 3</a> validated.
-			-->
+			<div class="containter">
+				&copy; All rights reserved.
+				<!--
+				<br />
+				Deze site is een schoolopdracht en <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> 
+				en is <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 3</a> validated.
+				-->
+			</div>
 		</div>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -3,13 +3,15 @@ include 'db/connection.php';
 $connection = openDB();
 include 'header.php'; 
 ?>
-<div id = "tekst">
-	<h2>Home</h2>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-		Integer aliquam arcu eu tincidunt aliquet. Sed vulputate viverra arcu at consequat. Ut rutrum ullamcorper libero et blandit.
-		Nullam semper arcu vel ultrices lobortis. Quisque porta enim et faucibus eleifend. Suspendisse nec elit in urna mollis euismod.
-		Nam condimentum ipsum mi, at tristique lorem pulvinar at. Quisque ullamcorper accumsan rhoncus. Sed sapien mi, tincidunt vitae vestibulum sed, lobortis ac quam.
-	</p>
+<div class="row">
+	<div class="span6 offset3">
+		<h2>Home</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Integer aliquam arcu eu tincidunt aliquet. Sed vulputate viverra arcu at consequat. Ut rutrum ullamcorper libero et blandit.
+			Nullam semper arcu vel ultrices lobortis. Quisque porta enim et faucibus eleifend. Suspendisse nec elit in urna mollis euismod.
+			Nam condimentum ipsum mi, at tristique lorem pulvinar at. Quisque ullamcorper accumsan rhoncus. Sed sapien mi, tincidunt vitae vestibulum sed, lobortis ac quam.
+		</p>
+	</div>
 </div>
 <?php include 'footer.php'; ?>
