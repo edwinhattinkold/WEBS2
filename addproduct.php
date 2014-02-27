fc9a99dfa0c054f935eeedbb8369572385d7555c
Edwin Hattink <edwinhattink@me.com>
2014-02-27 22:27:53 +0100

    update voor image upload

--- addproduct.php
+++ addproduct.php
@@ -5,36 +5,44 @@ include 'header.php';
 ?>
 	<div class="row">
 		<?php
+			include 'repositories/productrepository.php';			
+			$product = new Product();
 			echo "Product name: " . $_POST["name"] . "<br/>";
 			echo "Category: " . $_POST["category"] . "<br/>";
 			echo "Description: " . $_POST ["description"] . "<br/>";
 			echo "Price: " . $_POST ["price"] . "<br/>";
+			$product -> _set("name",$_POST["name"]);
+			$product -> _set("description",$_POST["description"]);
+			$product -> _set("price",$_POST["price"]);
+			$product -> _set("category_name",$_POST["category"]);
+			
 			if ($_FILES["file"]["error"] > 0)
 			{
 				echo "Error: " . $_FILES["file"]["error"] . "<br/>";
 			}
 			else
-			{				
-				if (file_exists("images/" . $_FILES["file"]["name"]))
+			{
+				$allowedExts = array("gif", "jpeg", "jpg", "png");
+				$temp = explode(".", $_FILES["file"]["name"]);
+				$extension = end($temp);
+				if (in_array($extension,$allowedExts))
 				{
-					echo $_FILES["file"]["name"] . " already exists. ";
+					if (file_exists("images/" . $_FILES["file"]["name"]))
+					{
+						echo $_FILES["file"]["name"] . " already exists. ";
+					}
+					else
+					{
+						move_uploaded_file($_FILES["file"]["tmp_name"],"images/" . $_FILES["file"]["name"]);
+					}
+					$product -> _set("image","images/".$_FILES["file"]["name"]);
+					echo "Uploaded image: <img src=\"images/".$_FILES["file"]["name"]."\" alt=\"Uploaded image\"/>";
 				}
 				else
 				{
-					move_uploaded_file($_FILES["file"]["tmp_name"],"images/" . $_FILES["file"]["name"]);
-					echo "Stored in: " . "images/" . $_FILES["file"]["name"];
+					echo "The file you uploaded is wrong."; 
 				}
-			}
-			
-			include 'repositories/productrepository.php';			
-			$product = new Product();
-			
-			$product -> _set("name",$_POST["name"]);
-			$product -> _set("description",$_POST["description"]);
-			$product -> _set("price",$_POST["price"]);
-			$product -> _set("image","images/".$_FILES["file"]["name"]);
-			$product -> _set("category_name",$_POST["category"]);
-			
+			}			
 			addProduct($connection,$product);
 		?>		
 	</div>
