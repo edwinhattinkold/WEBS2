3ed412e1134d4081646c5f1227b77d8d3c573c6a
thimh <thim.heider@gmail.com>
2014-02-28 11:35:09 +0100

    sticky footer

--- functions/footer.php
+++ functions/footer.php
@@ -1,15 +1,15 @@
-		<div id="footer">
-			<div class="containter">
-				&copy; All rights reserved.
-				<!--
-				<br />
-				Deze site is een schoolopdracht en <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> 
-				en is <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 3</a> validated.
-				-->
-			</div>
+		<div class="containter">
+			<nav class="navbar navbar-inverse navbar-fixed-bottom">
+				<p class="text-muted">
+					&copy; All rights reserved.
+					<!--
+					<br />
+					Deze site is een schoolopdracht en <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> 
+					en is <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS 3</a> validated.
+					-->
+				</p>
+			</nav>
 		</div>
 	</body>
 </html>
-<?php
-closeDB($connection);
-?>
+<?php closeDB($connection); ?>
