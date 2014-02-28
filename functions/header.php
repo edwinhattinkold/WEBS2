a5e8a3fba76c99a0407d20fbd320932df85d53c1
thimh <thim.heider@gmail.com>
2014-02-28 11:56:51 +0100

    sticky footer update

--- functions/header.php
+++ functions/header.php
@@ -7,39 +7,40 @@
 		</title>
 	</head>
 	<body>
-		<div id="bannerID">
-			<a href="index.php">
-					<img src="images/banner.png" width="275" height="172" alt="Banner Webshop" />
-					GAMESHOP ET
-			</a>
-		</div>
+		<div id="wrap">
+			<div id="bannerID">
+				<a href="index.php">
+						<img src="images/banner.png" width="275" height="172" alt="Banner Webshop" />
+						GAMESHOP ET
+				</a>
+			</div>
 
-		<nav class="navbar navbar-inverse" role="navigation">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-					</button>
-			</div>
-			
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<?php
-						include 'repositories/menurepository.php';
-						$menu = getAllUserMenus($connection);
-						
-						foreach($menu as &$menuitem)
-						{
-							$id = $menuitem -> _get("id");
-							$name = $menuitem -> _get("name");
-							$link = $menuitem -> _get("link");
-					?>
-							<li><a href="<?php echo $link?>"><?php echo $name?></a></li>
-					<?php
-						}
-					?>
-			</div>
-		</nav>
+			<nav class="navbar navbar-inverse" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+						</button>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<?php
+							include 'repositories/menurepository.php';
+							$menu = getAllUserMenus($connection);
+							
+							foreach($menu as &$menuitem)
+							{
+								$id = $menuitem -> _get("id");
+								$name = $menuitem -> _get("name");
+								$link = $menuitem -> _get("link");
+						?>
+								<li><a href="<?php echo $link?>"><?php echo $name?></a></li>
+						<?php
+							}
+						?>
+				</div>
+			</nav>
