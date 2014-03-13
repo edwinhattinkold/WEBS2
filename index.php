a48f8e5d6ded2cd0dccb927f65c5b588ebdb613d
Edwin Hattink <edwinhattink@me.com>
2014-03-13 15:36:24 +0100

    oei

--- index.php
+++ index.php
@@ -5,13 +5,8 @@ include 'functions/header.php';
 ?>
 <div class="row">
 	<div class="span6 offset3">
-		<h2>Home</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			Integer aliquam arcu eu tincidunt aliquet. Sed vulputate viverra arcu at consequat. Ut rutrum ullamcorper libero et blandit.
-			Nullam semper arcu vel ultrices lobortis. Quisque porta enim et faucibus eleifend. Suspendisse nec elit in urna mollis euismod.
-			Nam condimentum ipsum mi, at tristique lorem pulvinar at. Quisque ullamcorper accumsan rhoncus. Sed sapien mi, tincidunt vitae vestibulum sed, lobortis ac quam.
-		</p>
+		<?php
+		?>
 	</div>
 </div>
 <?php include 'functions/footer.php'; ?>
