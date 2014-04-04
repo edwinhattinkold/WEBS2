79fb126a8d548800021686fe7899bd13741bd31e
Edwin Hattink <edwinhattink@me.com>
2014-04-04 12:06:41 +0200

    register 100%

--- functions/validation.php
+++ functions/validation.php
@@ -8,4 +8,16 @@ function filterInt($input)
 {
 	return is_int($input);
 }
+
+function validateUsername($input,$connection)
+{
+	include_once 'repositories/userrepository.php';
+	$user = getUserByName($connection,$input);
+	return ($user->_get("username")!="");
+}
+
+function validatePassword($input,$reinput)
+{
+	return $input == $reinput;
+}
 ?>
--- pages/register.php
+++ pages/register.php
@@ -1,3 +1,4 @@
+<div class="container">
 <?php
 	include_once 'repositories/userrepository.php';
 	include_once 'functions/pwenc.php';
@@ -5,25 +6,40 @@
 	
 	if(filterEmail($_POST["email"]))
 	{
-		$user = new User();
-		$customer = new Customer();
-		
-		$user -> _set("username", $_POST["username"]);
-		$user -> _set("password", encrypt($_POST["pass"], $_POST["username"]));
-		$user -> _set("rights_right","customer");
-		
-		$customer -> _set("user_username", $_POST["username"]);
-		$customer -> _set("first_name", $_POST["first_name"]);
-		$customer -> _set("surname", $_POST["surname"]);
-		$customer -> _set("email", $_POST["email"]);
-		$customer -> _set("adress", $_POST["adress"]);
-		$customer -> _set("zipcode", $_POST["zipcode"]);
-		$customer -> _set("city", $_POST["city"]);
-		
-		addCustomer($connection,$customer,$user);
+		if (!validateUsername($_POST["username"],$connection))
+		{
+			if(validatePassword($_POST["pass"],$_POST["repass"]))
+			{
+				$user = new User();
+				$customer = new Customer();
+				
+				$user -> _set("username", $_POST["username"]);
+				$user -> _set("password", encrypt($_POST["pass"], $_POST["username"]));
+				$user -> _set("rights_right","customer");
+				
+				$customer -> _set("user_username", $_POST["username"]);
+				$customer -> _set("first_name", $_POST["first_name"]);
+				$customer -> _set("surname", $_POST["surname"]);
+				$customer -> _set("email", $_POST["email"]);
+				$customer -> _set("adress", $_POST["adress"]);
+				$customer -> _set("zipcode", $_POST["zipcode"]);
+				$customer -> _set("city", $_POST["city"]);
+				
+				addCustomer($connection,$customer,$user);
+			}
+			else
+			{
+				echo "Passwords don't match.";
+			}
+		}
+		else
+		{
+			echo "This username already exists.";
+		}
 	}
 	else
 	{
 		echo "Wrong email format.";
 	}
 ?>
+</div>
